90ce4186c1f292948af3382b9af3c8d7ca53585f
Lrs50 <lrs5@cin.ufpe.br>
2022-03-24 09:40:02 -0300

    version 1.0

---
+++ RPNStackerAdhoc.java
@@ -0,0 +1,54 @@
+import java.util.List;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Stack;  
+import java.lang.Math;
+
+public class RPNStackerAdhoc {
+    public static void main(String[] args) throws IOException{
+
+        Boolean logging = false;
+        List<String> content = Files.readAllLines(Paths.get("Avaliate.stk"));
+        Stack<Float> values= new Stack<Float>();
+        for (String string : content) {
+            try{
+                if(logging) System.out.println("Push "+Float.parseFloat(string));
+
+                values.push(Float.parseFloat(string));
+                
+
+            }
+            catch (NumberFormatException ex){
+                float b = values.pop();
+                float a = values.pop();
+                float result=0;
+
+                if(logging) System.out.println("Pop "+ a);
+                if(logging) System.out.println("Pop "+ b);
+                
+
+                if(string.equals("+")){
+                    result = a+b;
+                } else if (string.equals("-")){
+                    result = a-b;
+                }else if (string.equals("*")){
+                    result = a*b;
+                }else if (string.equals("/")){
+                    result = a/b;
+                }else if (string.equals("^")){
+                    result = (float)Math.pow(a, b);
+                }
+
+                if(!string.isEmpty()){
+                    values.push(result);
+                    if(logging) System.out.println("Push "+result);
+                }
+                
+
+            }
+        }
+
+        System.out.println("Resposta da avaliação = "+values.pop());
+    }
+}
